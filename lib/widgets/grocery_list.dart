7e3049e848d5cc0b3ec859e72fac51597787d0ba
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-10 17:16:54 +0200

    empty list message and delete item added

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -30,8 +30,41 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem groceryItem) {
+    setState(() {
+      _groceryItems.remove(groceryItem);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet!'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              '${_groceryItems[index].quantity}x'
+              ),
+          ),
+        )
+      );
+    }
+
     return Scaffold(
       appBar:AppBar(
         title: const Text('Your Groceries'),
@@ -42,20 +75,8 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            '${_groceryItems[index].quantity}x'
-            ),
-        )
-      ),
+      body: content,
     );
   }
+  
 }
